16b9fdd96cdce22ceb7742467c2ed4a6d6ddcddc
souporserious <tarnold@frame.io>
2021-08-30 10:17:42 -0700

    fix font

--- packages/site/src/pages/_document.tsx
+++ packages/site/src/pages/_document.tsx
@@ -10,8 +10,9 @@ class Document extends NextDocument {
     return (
       <Html>
         <Head>
+          <link href="https://fonts.gstatic.com" rel="preconnect" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400..900&family=Roboto+Mono:wght@500&display=swap"
             rel="stylesheet"
           />
         </Head>
