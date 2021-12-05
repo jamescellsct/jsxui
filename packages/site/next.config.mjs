ed1d2663e66cbfbe84643c1f3057a9ec84ac7ff1
souporserious <tarnold@frame.io>
2021-12-05 13:21:23 -0800

    more docs, hook up remote refresh

--- packages/site/next.config.mjs
+++ packages/site/next.config.mjs
@@ -1,35 +1,41 @@
 import withMDX from '@next/mdx'
 import gfm from 'remark-gfm'
 import shiki from 'rehype-shiki'
+import withRemoteRefresh from 'next-remote-refresh'
 
-export default withMDX({
-  extension: /\.mdx?$/,
-  options: {
-    providerImportSource: '@mdx-js/react',
-  },
-})({
-  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  webpack(config, options) {
-    // Load local MDX files and add MDXLayout to each file
-    config.module.rules.push({
-      test: /\.mdx$/,
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: '@mdx-js/loader',
-          options: {
-            remarkPlugins: [gfm],
-            rehypePlugins: [[shiki, { theme: 'src/theme.json' }]],
+export default withRemoteRefresh({
+  paths: [process.cwd() + '/src/docs'],
+  ignored: null,
+})(
+  withMDX({
+    extension: /\.mdx?$/,
+    options: {
+      providerImportSource: '@mdx-js/react',
+    },
+  })({
+    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    webpack(config, options) {
+      // Load local MDX files and add MDXLayout to each file
+      config.module.rules.push({
+        test: /\.mdx$/,
+        use: [
+          options.defaultLoaders.babel,
+          {
+            loader: '@mdx-js/loader',
+            options: {
+              remarkPlugins: [gfm],
+              rehypePlugins: [[shiki, { theme: 'src/theme.json' }]],
+            },
           },
-        },
-        // Use a custom loader to add MDXLayout to each MDX file
-        'mdx-loader.mjs',
-      ],
-    })
+          // Use a custom loader to add MDXLayout to each MDX file
+          'mdx-loader.mjs',
+        ],
+      })
 
-    return config
-  },
-})
+      return config
+    },
+  })
+)
--- packages/site/src/pages/_app.tsx
+++ packages/site/src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import { useRouter } from 'next/router'
+import { useRemoteRefresh } from 'next-remote-refresh/hook'
+
 import '../app.css'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  useRemoteRefresh({
+    shouldRefresh: (path) => path.includes(router.query.slug),
+  })
   return <Component {...pageProps} />
 }
